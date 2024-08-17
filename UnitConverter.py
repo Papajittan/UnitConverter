87e81d0af0120a89ccac818c2e7cd5daea9a7589
Papajittan <79068718+Papajittan@users.noreply.github.com>
2024-08-17 23:48:15 +0700

    Update UnitConverter.py (Optimize some shit)

--- UnitConverter.py
+++ UnitConverter.py
@@ -10,15 +10,18 @@ class UnitConverter:
         self.time = time
         self.force = force
         self.binary = binary
+        self.unitx = {"Speed":self.speed, "Pressure":self.pressure,"Time":self.time,"Force":self.force,"Binary":self.binary}
+        self.a = "Select a unit"
+        self.mainmenu()
     def clear(self):
-        self.value_inside.set("Select a unit")
-        self.value_inside2.set("Select a unit")
+        self.value_inside.set(self.a)
+        self.value_inside2.set(self.a)
         self.e1.delete(0, 'end')
         self.textvar.set("")
         self.textvar1.set("")
-    def checkfloat(self, string):
+    def checkfloat(self, num):
         try:
-            float(string)
+            float(num)
             return True
         except ValueError:
             return False
@@ -27,54 +30,33 @@ class UnitConverter:
         self.root2 = None
         self.textvar.set("")
         self.textvar1.set("")
-        self.mainloop()
+        self.mainmenu()
     def exit(self):
-        self.root2.destroy()
-        self.root2 = None
-        self.textvar.set("")
-        self.textvar1.set("")
-    def exit1(self):
-        self.root.destroy()
-        self.root = None
+        if(self.root is None):
+            self.root2.destroy()
+            self.root2 = None
+            self.textvar.set("")
+            self.textvar1.set("")
+        else:
+            self.root.destroy()
+            self.root = None
     def calculate(self):
-            v1 = self.e1.get()
-            v2 = self.value_inside.get()
-            v3 = self.value_inside2.get()
-            v4 = self.y
-            v5 = v4[v2]
-            v6 = v4[v3]
-            if(self.checkfloat(v1)):
-                v1 = float(v1)
-                v7 = v1*v5
-                v8 = v7/v6
-                self.textvar.set(v8)
+            if(self.checkfloat(self.e1.get()) and self.value_inside.get() != self.a and self.value_inside2.get() != self.a):
+                final = float(self.e1.get())*self.y[self.value_inside.get()]/self.y[self.value_inside2.get()]
+                self.textvar.set(final)
                 self.textvar1.set("The result is:")
             else:
                 self.e1.delete(0, 'end')
     def changew(self, unit):
-        if(self.root is not None):
-            self.root.destroy()
-            self.root = None
-        if(self.root2 is None):
-            self.root2 = Tk()
+        self.root.destroy()
+        self.root = None
+        self.root2 = Tk()
+        self.photoe = PhotoImage(file = 'unnamedd.png')
+        self.root2.wm_iconphoto(False, self.photoe)
         self.root2.configure(bg="gray25")
         self.root2.title("Converting " + unit)
-        if(unit == "Speed"):
-            self.y = self.speed
-            self.x = list(speed.keys())
-        elif(unit == "Pressure"):
-            self.y = self.pressure
-            self.x = list(pressure.keys())
-        elif(unit == "Time"):
-            self.y = self.time
-            self.x = list(time.keys())
-        elif(unit == "Binary"):
-            self.y = self.binary
-            self.x = list(binary.keys())
-        elif(unit == "Force"):
-            self.y = self.force
-            self.x = list(force.keys())
-        x2 = self.x
+        self.y = dict(self.unitx[unit])
+        self.x = list(self.y.keys())
         text9 = Label(text = "Converting "+ unit,fg='ghost white',font='Helvetica 12',bg='MediumOrchid4')
         text9.grid(row=0,column=0,columnspan=4,ipadx=70,ipady=2,pady=1) 
         label1 = Label(text = "Insert a number here:",fg='light cyan',font='Helvetica 10',bg="dark slate grey")
@@ -85,13 +67,13 @@ class UnitConverter:
         label2.grid(row = 2,column = 0,columnspan=2,ipadx=30,ipady=0,pady=1)
         self.value_inside = StringVar()
         self.value_inside2 = StringVar()
-        self.value_inside.set("Select a unit") 
+        self.value_inside.set(self.a) 
         self.option_menu = OptionMenu(self.root2, self.value_inside, *self.x)
         self.option_menu.grid(column=2, row=2, sticky=W)
         label3 = Label(text = "Convert To:",fg='light cyan',font='Helvetica 10',bg="dark slate grey")
         label3.grid(row = 3,column = 0,columnspan=2,ipadx=40,ipady=0,pady=1)
-        self.value_inside2.set("Select a unit")
-        self.option_menu1 = OptionMenu(self.root2, self.value_inside2, *x2)
+        self.value_inside2.set(self.a)
+        self.option_menu1 = OptionMenu(self.root2, self.value_inside2, *self.x)
         self.option_menu1.grid(column=2, row=3, sticky=W)
         self.textvar = StringVar()
         self.textvar1 = StringVar()
@@ -108,12 +90,13 @@ class UnitConverter:
         button8=Button(text='Exit',compound=TOP,fg='grey9',bg='MediumPurple1',command = self.exit)
         button8.grid(row=6,column=1)
         self.root2.mainloop()
-    def mainloop(self):
-        if(self.root is None):
-            self.root = Tk()
+    def mainmenu(self):
+        self.root = Tk()
         self.root.title("Unit Converter")
+        self.photoe = PhotoImage(file = 'unnamedd.png')
+        self.root.wm_iconphoto(False, self.photoe)
         self.root.configure(bg="black")
-        text = Label(text = "Unit Converter",fg='black',font='Helvetica 12',bg='azure')
+        text = Label(text = "Unit Converter",fg='azure',font='Helvetica 12',bg='SlateBlue4')
         text.grid(row=0,column=0,columnspan=4,ipadx=135,ipady=2,pady=1) 
         text2 = Label(text = "Select the unit category!",fg='gray84',font='Arail 10',bg='purple')
         text2.grid(row=1,column=0,columnspan=4,ipadx=50,ipady=1,pady=0)
@@ -132,7 +115,7 @@ class UnitConverter:
         button4.grid(row=3,column=0,columnspan=2)
         button5  =Button(text='Force',image=photo5,compound=TOP,fg='grey9',bg='MediumPurple1',command=lambda: self.changew("Force"))
         button5.grid(row=3,column=1,columnspan=2)
-        button11 =Button(text='Exit',fg='grey9',bg='plum2',command= self.exit1)
+        button11 =Button(text='Exit',fg='lavender',bg='midnight blue',command= self.exit)
         button11.grid(row = 4, column = 1,ipadx=50)
         self.root.mainloop()
 speed = {"kmh": ipy.constants.kmh, "mph": ipy.constants.mph, "mach": ipy.constants.mach, "speed of sound": ipy.constants.speed_of_sound , "knot": ipy.constants.knot, "ms": 1.0}
@@ -141,4 +124,3 @@ time = {"minute": ipy.constants.minute, "hour": ipy.constants.hour, "day": ipy.c
 force = {"dyn":ipy.constants.dyn,"dyne":ipy.constants.dyne,"lbf":ipy.constants.lbf,"pound force":ipy.constants.pound_force,"kgf":ipy.constants.kgf,"kilogram force": ipy.constants.kilogram_force, "newtons": 1.0 }
 binary = {"kibi":ipy.constants.kibi, "mebi":ipy.constants.mebi, "gibi":ipy.constants.gibi, "tebi":ipy.constants.tebi, "pebi":ipy.constants.pebi, "exbi":ipy.constants.exbi, "zebi":ipy.constants.zebi, "yobi":ipy.constants.yobi}
 converter = UnitConverter(speed, pressure, time, force, binary)
-converter.mainloop()
